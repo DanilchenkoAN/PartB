7a4120004245b5672dc8e9dae9bd58df53dc7179
ANDREY DANILCHENKO <100102296+DanilchenkoAN@users.noreply.github.com>
2022-05-11 21:02:16 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5340188876:AAH0Xzay0Y8n5i1i629LIaqtF-MkLdmfNf8'
+keys = {
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+    'Доллар': 'USD',
+    'Йена': 'JPY',
+    'Франк': 'CHF',
+    'Фунт': 'GBP',
+}
